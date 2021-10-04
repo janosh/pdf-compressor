09bb7eda621e5ff92791bac467d78808956aef71
MarcinGladkowski <marcingladkowski@gmail.com>
2021-10-04 21:43:17 +0200

    create example temporary env file

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,21 @@
-from pdf_compressor.utils import sizeof_fmt
+from pdf_compressor.utils import sizeof_fmt, load_dotenv
+from os.path import abspath, dirname, expanduser, getsize, isfile, splitext
+import os
 
 
 def test_sizeof_fmt():
-
     assert sizeof_fmt(123456) == "120.6 KB"
 
     assert sizeof_fmt(123456789, 3) == sizeof_fmt(123456789, prec=3) == "117.738 MB"
+
+
+def test_load_env():
+    f = open(".env", "a")
+    f.write("APP_TEST=test")
+    f.close()
+
+    test_file = f"{dirname(__file__)}/.env"
+
+    load_dotenv(filepath=test_file)
+
+    assert os.getenv('APP_TEST') == 'test'

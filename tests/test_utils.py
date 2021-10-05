93debedcf2a022db20acf3f05036f94644f85032
MarcinGladkowski <marcingladkowski@gmail.com>
2021-10-05 06:40:32 +0200

    helper functions to create and remove test env files

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,16 @@
 from pdf_compressor.utils import sizeof_fmt, load_dotenv
-from os.path import abspath, dirname, expanduser, getsize, isfile, splitext
+from os.path import dirname
 import os
+
+
+def file_test_creator(filename: str, content: str):
+    f = open(filename, 'a')
+    f.write(content)
+    f.close()
+
+
+def file_remove(filename: str):
+    os.remove(filename)
 
 
 def test_sizeof_fmt():
@@ -10,12 +20,36 @@ def test_sizeof_fmt():
 
 
 def test_load_env():
-    f = open(".env", "a")
-    f.write("APP_TEST=test")
-    f.close()
+    test_file = f"{dirname(__file__)}/.env.test"
 
-    test_file = f"{dirname(__file__)}/.env"
+    file_test_creator(test_file, "APP_TEST=test")
 
     load_dotenv(filepath=test_file)
 
+    file_remove(test_file)
+
     assert os.getenv('APP_TEST') == 'test'
+
+
+def test_load_commented_env():
+    test_file = f"{dirname(__file__)}/.env.test_commented"
+
+    file_test_creator(test_file, "#APP_TEST=test")
+
+    load_dotenv(filepath=test_file)
+
+    file_remove(test_file)
+
+    assert os.getenv('COMMENT') is None
+
+
+def test_load_empty_env():
+    test_file = f"{dirname(__file__)}/.env.empty"
+
+    file_test_creator(test_file, "")
+
+    load_dotenv(filepath=test_file)
+
+    file_remove(test_file)
+
+    assert os.getenv('APP_EMPTY') is None

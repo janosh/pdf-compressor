6e432965521890d5fdf1a4c430a8978ca81911f9
Janosh Riebesell <janosh.riebesell@gmail.com>
2021-11-09 19:40:08 +0100

    refactor sizeof_fmt() to si_fmt(), less verbose CLI

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,8 +1,12 @@
-from pdf_compressor.utils import sizeof_fmt
+from pdf_compressor.utils import si_fmt
 
 
-def test_sizeof_fmt():
+def test_greek_fmt():
 
-    assert sizeof_fmt(123456) == "120.6 KB"
+    assert si_fmt(123456) == "120.6K"
 
-    assert sizeof_fmt(123456789, 3) == sizeof_fmt(123456789, prec=3) == "117.738 MB"
+    assert si_fmt(12345678, fmt_spec=">6.2f", sep=" ") == " 11.77 M"
+
+    assert si_fmt(0.00123, fmt_spec=".3g", binary=False) == "1.23m"
+
+    assert si_fmt(0.00000123, fmt_spec="5.1f", sep=" ") == "  1.3 μ"
